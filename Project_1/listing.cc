5d1c2263a1b9713de82fd87608bf8689828790ee
Jen McClintock <jenibobo04@gmail.com>
2022-06-16 18:25:40 -0400

    Tokens and scanner file added to. Edited the listing.cc file

--- Project_1/listing.cc
+++ Project_1/listing.cc
@@ -6,51 +6,71 @@
 
 #include <cstdio>
 #include <string>
+#include <queue>
+
 
 using namespace std;
 
 #include "listing.h"
 
 static int lineNumber;
-static string error = "";
+// static string error = "";
 static int totalErrors = 0;
+static queue<string> error;
+static int error_arr[] = {0, 0, 0}; // index_0=lex_err, index_1=syntax_err, index_2=sem_err
+
+// static int lex_err = 0; // Added var for lexical error count
+// static int syntax_err = 0; // Added var for syntax error count
+// static int sem_err = 0; // Added var for semantic error count
 
 static void displayErrors();
 
-void firstLine()
-{
+void firstLine() {
 	lineNumber = 1;
 	printf("\n%4d  ",lineNumber);
 }
 
-void nextLine()
-{
+void nextLine() {
 	displayErrors();
 	lineNumber++;
 	printf("%4d  ",lineNumber);
 }
 
-int lastLine()
-{
+int lastLine() {
 	printf("\r");
 	displayErrors();
 	printf("     \n");
+
+	totalErrors = error_arr[0] + error_arr[1] + error_arr[2];
+
+	if (totalErrors == 0) {
+		printf("Compiled Sucessfully!\n");
+	} else {
+		printf(" Lexical Errors: %4d\n", error_arr[0]);
+		printf(" Sytax Errors: %4d\n", error_arr[1]);
+		printf(" Semantic Errors: %4d\n", error_arr[2]);
+	}
+
+	printf("%4d", totalErrors);
 	return totalErrors;
 }
     
-void appendError(ErrorCategories errorCategory, string message)
-{
-	string messages[] = { "Lexical Error, Invalid Character ", "",
-		"Semantic Error, ", "Semantic Error, Duplicate Identifier: ",
-		"Semantic Error, Undeclared " };
+void appendError(ErrorCategories errorCategory, string message) {
+	string messages[] = { 
+		"Lexical Error, Invalid Character ", 
+		"Syntax Error, ",
+		"Semantic Error, ",
+		"Semantic Error, Duplicate Identifier: ",
+		"Semantic Error, Undeclared " 
+		};
+	error.push(messages[errorCategory] + message+"\n");
+	error_arr[errorCategory]++;
 
-	error = messages[errorCategory] + message;
-	totalErrors++;
 }
 
-void displayErrors()
-{
-	if (error != "")
-		printf("%s\n", error.c_str());
-	error = "";
+void displayErrors() {
+	while (!error.empty()){
+		printf("%s\n", error.front().c_str());
+		error.pop();
+	}
 }
