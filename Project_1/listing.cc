b6ed309ca8f081615862731e9766e2b756bd8af8
Jen <71782270+Jenibobo@users.noreply.github.com>
2022-06-23 07:30:29 -0400

    Update listing.cc

--- Project_1/listing.cc
+++ Project_1/listing.cc
@@ -6,51 +6,89 @@
 
 #include <cstdio>
 #include <string>
+#include <queue>
+
 
 using namespace std;
 
 #include "listing.h"
 
 static int lineNumber;
-static string error = "";
 static int totalErrors = 0;
+static queue<string> errors;
+// static int error_arr[] = {0, 0, 0}; // index_0=lex_err, index_1=syntax_err, index_2=sem_err
+static int lex_err = 0;
+static int syntax_err = 0;
+static int sem_err = 0;
 
 static void displayErrors();
 
-void firstLine()
-{
+void firstLine() {
 	lineNumber = 1;
 	printf("\n%4d  ",lineNumber);
 }
 
-void nextLine()
-{
+void nextLine() {
 	displayErrors();
 	lineNumber++;
 	printf("%4d  ",lineNumber);
 }
 
-int lastLine()
-{
+int lastLine() {
 	printf("\r");
 	displayErrors();
 	printf("     \n");
+
+	// totalErrors = error_arr[0] + error_arr[1] + error_arr[2];
+	totalErrors = lex_err + syntax_err + sem_err;
+
+	if (totalErrors == 0) {
+		printf("Compiled Sucessfully!\n");
+	} else {
+		// printf(" Lexical Errors: %4d\n", error_arr[0]);
+		// printf(" Sytax Errors: %4d\n", error_arr[1]);
+		// printf(" Semantic Errors: %4d\n", error_arr[2]);
+
+		printf(" Lexical Errors: %4d\n", lex_err);
+		printf(" Sytax Errors: %4d\n", syntax_err);
+		printf(" Semantic Errors: %4d\n", sem_err);
+	}
+
+	printf("%4d", totalErrors);
 	return totalErrors;
 }
     
-void appendError(ErrorCategories errorCategory, string message)
-{
-	string messages[] = { "Lexical Error, Invalid Character ", "",
-		"Semantic Error, ", "Semantic Error, Duplicate Identifier: ",
-		"Semantic Error, Undeclared " };
+void appendError(ErrorCategories errorCategory, string message) {
+	string messages[] = { 
+		"Lexical Error, Invalid Character ", 
+		"Syntax Error, ",
+		"Semantic Error, ",
+		"Semantic Error, Duplicate Identifier: ",
+		"Semantic Error, Undeclared " 
+		};
+	errors.push(messages[errorCategory] + message + "\n");
+	// printf("%s\n", message.c_str());
+	// printf("%s\n", (messages[errorCategory] + message).c_str());
+	// error_arr[errorCategory]++;
+	
+	switch (errorCategory) {
+		case 0:
+			lex_err++;
+			break;
+		case 1:
+			syntax_err++;
+			break;
+		case 2:
+			sem_err++;
+			break;
+	}
+	// printf("%s\n", (messages[errorCategory] + message).c_str());
 
-	error = messages[errorCategory] + message;
-	totalErrors++;
 }
 
-void displayErrors()
-{
-	if (error != "")
-		printf("%s\n", error.c_str());
-	error = "";
+void displayErrors() {
+	while (!errors.empty()){
+		printf("%s\n", errors.front().c_str());
+		errors.pop();
+	}
 }
